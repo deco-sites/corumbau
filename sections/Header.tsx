03294a49b438a92af7487d9aeed471fe48449f4f
Matheus Trindade <matheus.trindade5201@hotmail.com>
2024-08-03 16:14:09 -0300

    Release localhost environment with 11 changes by Matheus Trindade

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,6 +1,9 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import Icon from "../components/ui/Icon.tsx";
+import { useScript } from "deco/hooks/useScript.ts";
+import { useSignal } from "@preact/signals";
+import { FnContext } from "deco/types.ts";
 
 export interface CTA {
   id?: string;
@@ -18,6 +21,10 @@ export interface Nav {
     links: {
       label?: string;
       url?: string;
+      children?: {
+        label?: string;
+        url?: string;
+      }[];
     }[];
     buttons: CTA[];
   };
@@ -41,25 +48,44 @@ export default function Header({
       { id: "change-me-2", href: "/", text: "Change me", outline: true },
     ],
   },
-}: Nav) {
+  pathname,
+}: ReturnType<typeof loader>) {
+  function script() {
+    const styles = ["bg-white"];
+    const handleScroll = () => {
+      if (globalThis.scrollY > 260) {
+        globalThis.document.getElementById("header")?.classList.add(...styles);
+      } else {
+        globalThis.document.getElementById("header")?.classList.remove(
+          ...styles,
+        );
+      }
+    };
+
+    globalThis.addEventListener("scroll", handleScroll);
+  }
+
   return (
-    <nav class="drawer drawer-end">
+    <nav id={"header"} class="drawer drawer-end fixed top-0 z-50">
       <input id="mobile-drawer-nav" type="checkbox" class="drawer-toggle" />
 
       {/* main content */}
-      <div class="drawer-content container lg:px-0 px-4 flex gap-8 items-center justify-between py-4">
+      <div
+        class={`drawer-content container lg:px-0 px-4 flex gap-8 items-center justify-between py-4`}
+      >
         <a href="/">
           <Image src={logo.src || ""} width={100} height={28} alt={logo.alt} />
         </a>
 
         <div class="hidden items-center justify-between lg:flex w-full">
-          <ul class="flex">
+          <ul class="flex gap-5">
             {navigation.links.map((link) => (
-              <li>
+              <li class="group relative">
+                <div class="absolute h-[5px] bg-primary w-0 transition-all duration-300 group-hover:w-full -top-[15px]" />
                 <a
                   href={link.url}
                   aria-label={link.label}
-                  class="link no-underline hover:underline p-4"
+                  class="link no-underline"
                 >
                   {link.label}
                 </a>
@@ -137,6 +163,20 @@ export default function Header({
           </ul>
         </div>
       </aside>
+      <script
+        type="module"
+        dangerouslySetInnerHTML={{ __html: useScript(script) }}
+      />
     </nav>
   );
 }
+
+export const loader = (props: Nav, req: Request, _ctx: FnContext) => {
+  console.log(req.url);
+  const pathname = new URL(req.url).pathname;
+
+  return {
+    ...props,
+    pathname,
+  };
+};
