1921c85888b0e97e0b6f595cd9890096e4fd9591
Matheus Trindade <matheus.trindade5201@hotmail.com>
2024-08-04 09:47:47 -0300

    Release localhost environment with 11 changes by Matheus Trindade

--- sections/TextBlock.tsx
+++ sections/TextBlock.tsx
@@ -5,7 +5,7 @@ export interface Props {
 
 export default function TextBlock({ text }: Props) {
     return (
-        <div class="w-full flex justify-center pt-10">
+        <div class="w-full flex justify-center pt-10 fade-in-down">
             <div class="w-full max-w-[90%] sm:max-w-[80%] md:max-w-[60%] lg:max-w-[70%]">
                 <div
                     dangerouslySetInnerHTML={{ __html: text }}
