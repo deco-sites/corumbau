69c5fc20d87b8804acafef7d24cfd976052c7c8d
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2024-08-06 00:19:35 -0300

    refactor: add missing properties

--- sections/LabelText.tsx
+++ sections/LabelText.tsx
@@ -7,31 +7,58 @@ export interface Props {
     title: string;
     /**
      * @title Descrição
+     * @format textarea
      */
     description: string;
     /**
+     * @title Alinhamento da Descrição
+     */
+    alignment?: "Esquerda" | "Direita";
+    /**
+     * @title Tamanho da Descrição
+     */
+    size?: "Medio" | "Padrão";
+    /**
      * @title Botão
      */
-    button?: { href: string; text: string };
+    button?: { href?: string; text?: string };
 }
 
-export default function LabelText({ description, title, button }: Props) {
+const ALIGNMENT = {
+    Direita: "md:text-right",
+    Esquerda: "md:text-left",
+};
+const SIZE = {
+    Medio: "text-lg",
+    Padrão: "text-xl lg:text-[22px]",
+};
+
+export default function LabelText(
+    { description, title, button, alignment = "Direita", size = "Padrão" }:
+        Props,
+) {
+    const hasButton = button && button.href && button.text;
+
     return (
-        <div class="w-full flex justify-center pt-10 fade-in-down">
+        <div class="w-full  flex justify-center pt-10 fade-in-down">
             <div class="flex flex-col md:flex-row  gap-5 md:gap-1 md:items-center flex-wrap xl:flex-nowrap text-neutral w-[90%] xl:w-[85%] md:justify-between">
-                <div class="flex flex-col md:flex-row md:items-center md:justify-between">
+                <div class="flex flex-col md:flex-row md:items-start md:justify-between flex-grow md:flex-wrap">
                     <p class="pl-5 md:pl-9 border-l-[5px] border-neutral w-fit text-[32px] font-light uppercase md:w-1/2 tracking-[.2em]">
                         {title}
                     </p>
-                    <p
+                    <div
                         class={`md:pl-9 md:w-1/2 ${
-                            !button && "min-w-[70%] flex-grow"
-                        } text-xl font-extralight lg:text-[22px] md:text-right`}
+                            !hasButton && "flex-grow"
+                        } font-extralight ${SIZE[size]} ${
+                            ALIGNMENT[alignment]
+                        }`}
                     >
-                        {description}
-                    </p>
+                        <p>
+                            {description}
+                        </p>
+                    </div>
                 </div>
-                {button && (
+                {button && button.href && button.text && (
                     <CommonButton
                         href={button.href}
                         text={button.text}
