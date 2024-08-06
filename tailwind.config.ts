4a7ab69d57ab44857ebc49404e8377ab1dcc785e
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2024-08-06 00:19:52 -0300

    chore: styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ export default {
   theme: {
     container: { center: true },
     extend: {
+      screens: {
+        "mid": "990px",
+      },
       spacing: {
         "35px": "35px",
         "3px": "3px",
@@ -15,9 +18,12 @@ export default {
         sliding: "sliding 30s linear infinite",
       },
       backgroundImage: {
-        'base-gradient': 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(255, 255, 255, 0))',
-        'inverted-gradient': 'linear-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8))',
-        'inverted-gradient-wider': 'linear-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.8))'
+        "base-gradient":
+          "linear-gradient(rgba(0, 0, 0, 0.9), rgba(255, 255, 255, 0))",
+        "inverted-gradient":
+          "linear-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8))",
+        "inverted-gradient-wider":
+          "linear-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.8))",
       },
       keyframes: {
         sliding: {
@@ -25,9 +31,10 @@ export default {
           "100%": { transform: "translateX(-50%)" },
         },
       },
-       boxShadow: {
-        'base': '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-      }
+      boxShadow: {
+        "base":
+          "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+      },
     },
   },
 };
