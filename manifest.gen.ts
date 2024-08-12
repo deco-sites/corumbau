b12b469f269bad224fba898f0e5394366378befd
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2024-08-12 18:38:57 -0300

    feat: integrate contact form

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,53 +2,57 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$$$$$$0 from "./apps/deco/resend.ts";
+import * as $$$$$$$$$$$1 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$2 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Carousel.tsx";
-import * as $$$$$$1 from "./sections/ContactForm.tsx";
-import * as $$$$$$2 from "./sections/CookieConsent.tsx";
-import * as $$$$$$3 from "./sections/CtaBanner.tsx";
-import * as $$$$$$4 from "./sections/DescriptiveBanner.tsx";
-import * as $$$$$$5 from "./sections/ExtensiveText.tsx";
-import * as $$$$$$6 from "./sections/Footer.tsx";
-import * as $$$$$$7 from "./sections/Gallery.tsx";
-import * as $$$$$$8 from "./sections/Header.tsx";
-import * as $$$$$$9 from "./sections/Hero.tsx";
-import * as $$$$$$10 from "./sections/HeroVideo.tsx";
-import * as $$$$$$11 from "./sections/IconTileList.tsx";
-import * as $$$$$$12 from "./sections/ImageShelf.tsx";
-import * as $$$$$$13 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$14 from "./sections/LabelText.tsx";
-import * as $$$$$$15 from "./sections/Map.tsx";
-import * as $$$$$$16 from "./sections/Testimonials.tsx";
-import * as $$$$$$17 from "./sections/TextBlock.tsx";
-import * as $$$$$$18 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$1 from "./sections/Component.tsx";
+import * as $$$$$$2 from "./sections/ContactForm.tsx";
+import * as $$$$$$3 from "./sections/CookieConsent.tsx";
+import * as $$$$$$4 from "./sections/CtaBanner.tsx";
+import * as $$$$$$5 from "./sections/DescriptiveBanner.tsx";
+import * as $$$$$$6 from "./sections/ExtensiveText.tsx";
+import * as $$$$$$7 from "./sections/Footer.tsx";
+import * as $$$$$$8 from "./sections/Gallery.tsx";
+import * as $$$$$$9 from "./sections/Header.tsx";
+import * as $$$$$$10 from "./sections/Hero.tsx";
+import * as $$$$$$11 from "./sections/HeroVideo.tsx";
+import * as $$$$$$12 from "./sections/IconTileList.tsx";
+import * as $$$$$$13 from "./sections/ImageShelf.tsx";
+import * as $$$$$$14 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$15 from "./sections/LabelText.tsx";
+import * as $$$$$$16 from "./sections/Map.tsx";
+import * as $$$$$$17 from "./sections/Testimonials.tsx";
+import * as $$$$$$18 from "./sections/TextBlock.tsx";
+import * as $$$$$$19 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
     "site/sections/Carousel.tsx": $$$$$$0,
-    "site/sections/ContactForm.tsx": $$$$$$1,
-    "site/sections/CookieConsent.tsx": $$$$$$2,
-    "site/sections/CtaBanner.tsx": $$$$$$3,
-    "site/sections/DescriptiveBanner.tsx": $$$$$$4,
-    "site/sections/ExtensiveText.tsx": $$$$$$5,
-    "site/sections/Footer.tsx": $$$$$$6,
-    "site/sections/Gallery.tsx": $$$$$$7,
-    "site/sections/Header.tsx": $$$$$$8,
-    "site/sections/Hero.tsx": $$$$$$9,
-    "site/sections/HeroVideo.tsx": $$$$$$10,
-    "site/sections/IconTileList.tsx": $$$$$$11,
-    "site/sections/ImageShelf.tsx": $$$$$$12,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$13,
-    "site/sections/LabelText.tsx": $$$$$$14,
-    "site/sections/Map.tsx": $$$$$$15,
-    "site/sections/Testimonials.tsx": $$$$$$16,
-    "site/sections/TextBlock.tsx": $$$$$$17,
-    "site/sections/Theme/Theme.tsx": $$$$$$18,
+    "site/sections/Component.tsx": $$$$$$1,
+    "site/sections/ContactForm.tsx": $$$$$$2,
+    "site/sections/CookieConsent.tsx": $$$$$$3,
+    "site/sections/CtaBanner.tsx": $$$$$$4,
+    "site/sections/DescriptiveBanner.tsx": $$$$$$5,
+    "site/sections/ExtensiveText.tsx": $$$$$$6,
+    "site/sections/Footer.tsx": $$$$$$7,
+    "site/sections/Gallery.tsx": $$$$$$8,
+    "site/sections/Header.tsx": $$$$$$9,
+    "site/sections/Hero.tsx": $$$$$$10,
+    "site/sections/HeroVideo.tsx": $$$$$$11,
+    "site/sections/IconTileList.tsx": $$$$$$12,
+    "site/sections/ImageShelf.tsx": $$$$$$13,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$14,
+    "site/sections/LabelText.tsx": $$$$$$15,
+    "site/sections/Map.tsx": $$$$$$16,
+    "site/sections/Testimonials.tsx": $$$$$$17,
+    "site/sections/TextBlock.tsx": $$$$$$18,
+    "site/sections/Theme/Theme.tsx": $$$$$$19,
   },
   "apps": {
-    "site/apps/decohub.ts": $$$$$$$$$$$0,
-    "site/apps/site.ts": $$$$$$$$$$$1,
+    "site/apps/deco/resend.ts": $$$$$$$$$$$0,
+    "site/apps/decohub.ts": $$$$$$$$$$$1,
+    "site/apps/site.ts": $$$$$$$$$$$2,
   },
   "name": "site",
   "baseUrl": import.meta.url,
--- sections/ContactForm.tsx
+++ sections/ContactForm.tsx
@@ -1,4 +1,7 @@
 import IframeModal from "../components/ui/IframeModal.tsx";
+import { useComponent } from "../sections/Component.tsx";
+import { AppContext } from "site/apps/site.ts";
+import type { AppContext as ResendApp } from "apps/resend/mod.ts";
 
 interface Policy {
     /**
@@ -49,6 +52,91 @@ export interface Props {
      * @description Definirá os placeholders que será exibido em cada input do formulário
      */
     formLabels: FormLabel;
+    sentEmail?: boolean;
+}
+
+export async function action(
+    props: Props,
+    req: Request,
+    ctx: AppContext & ResendApp,
+) {
+    console.log(props);
+
+    const formData = await req.formData();
+
+    const name = formData.get("name") || "";
+    const email = formData.get("email") || "";
+    const phone = formData.get("phone") || "";
+    const message = formData.get("message") || "";
+
+    await ctx.invoke.resend.actions.emails.send({
+        html: `
+        <!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template de E-mail</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        .container {
+            max-width: 600px;
+            margin: auto;
+            padding: 20px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            background-color: #f9f9f9;
+        }
+        h1 {
+            font-size: 24px;
+            color: #333;
+        }
+        p {
+            font-size: 16px;
+            color: #555;
+            line-height: 1.5;
+        }
+        .info {
+            margin-bottom: 10px;
+        }
+        .info strong {
+            display: block;
+            margin-bottom: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Nova Mensagem de Contato</h1>
+        <p>Você recebeu uma nova mensagem através do formulário de contato. Veja abaixo os detalhes:</p>
+        <div class="info">
+            <strong>Nome:</strong>
+            <p>${name}</p>
+        </div>
+        <div class="info">
+            <strong>E-mail:</strong>
+            <p>${email}</p>
+        </div>
+        <div class="info">
+            <strong>Telefone:</strong>
+            <p>${phone}</p>
+        </div>
+        <div class="info">
+            <strong>Mensagem:</strong>
+            <p>${message}</p>
+        </div>
+    </div>
+</body>
+</html>
+
+        `,
+        subject: "Formulário de contato",
+    });
+
+    return { ...props, sentEmail: true };
 }
 
 const DEFAULT_PROPS: Props = {
@@ -68,13 +156,19 @@ const DEFAULT_PROPS: Props = {
 };
 
 export default function ContactForm(props: Props) {
-    const { policy, formLabels } = { ...DEFAULT_PROPS, ...props };
+    const { policy, formLabels, sentEmail } = { ...DEFAULT_PROPS, ...props };
 
     return (
-        <div class="w-full flex justify-center pt-10 fade-in-down">
+        <div class="w-full flex justify-center pt-10 fade-in-down" id="wrapper">
             <div class="relative w-[90%] xl:w-[85%]">
-                <form class="w-full mt-24 font-extralight flex flex-col gap-8 text-base-gray items-start">
-                    <div class="w-full flex flex-col gap-8">
+                <form
+                    hx-post={useComponent(import.meta.url, props)}
+                    hx-trigger="submit"
+                    hx-swap="outerHTML"
+                    hx-target="#wrapper"
+                    class="w-full mt-24 font-extralight flex flex-col gap-8 text-base-gray items-start"
+                >
+                    <div class="w-full flex flex-col gap-8 md:flex-row">
                         <div class="w-full">
                             <input
                                 class="pb-5 border-b border-base-gray w-full focus:border-b-primary focus:outline-none text-base-gray"
@@ -82,6 +176,7 @@ export default function ContactForm(props: Props) {
                                 name="name"
                                 placeholder={`${formLabels.name}*`}
                                 arial-label="Seu nome"
+                                required
                             />
                         </div>
                         <div class="w-full">
@@ -91,6 +186,7 @@ export default function ContactForm(props: Props) {
                                 name="email"
                                 placeholder={`${formLabels.email}*`}
                                 arial-label="Seu email"
+                                required
                             />
                         </div>
                         <div class="w-full">
@@ -100,6 +196,7 @@ export default function ContactForm(props: Props) {
                                 name="phone"
                                 placeholder={`${formLabels.phone}*`}
                                 arial-label="Seu telefone"
+                                required
                             />
                         </div>
                     </div>
@@ -109,6 +206,7 @@ export default function ContactForm(props: Props) {
                                 class="p-9 bg-accent w-full h-80 text-area focus:outline-none"
                                 name="message"
                                 placeholder={`${formLabels.message}*`}
+                                required
                             >
                             </textarea>
                             <span>
@@ -138,6 +236,11 @@ export default function ContactForm(props: Props) {
                             </p>
                         </div>
                     </div>
+                    {sentEmail && (
+                        <div class="bg-primary p-4 rounded-sm text-white">
+                            Email enviado com sucesso!
+                        </div>
+                    )}
                     <button
                         class={`font-normal mt-7 text-center text-base bg-primary text-white rounded-sm py-[15px] transition-all duration-300 px-5 min-w-36 hover:bg-secondary`}
                     >
