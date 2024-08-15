605c94e2b9f732b5cafcbe47ec8ba2479ad5e7da
decobot <capy@deco.cx>
2024-08-15 20:56:52 -0300

    chore: update text components

--- sections/LabelText.tsx
+++ sections/LabelText.tsx
@@ -4,13 +4,14 @@ import { Placeholder } from "site/components/ui/SectionPlaceholder.tsx";
 export interface Props {
     /**
      * @title Titulo
+     * @format rich-text
      */
     title: string;
     /**
      * @title Descrição
      * @format textarea
      */
-    description: string;
+    description?: string;
     /**
      * @title Alinhamento da Descrição
      */
@@ -32,8 +33,6 @@ export interface Props {
 
 const DEFAULT_PROPS: Props = {
     title: "Título da Seção",
-    description:
-        "Esta é uma descrição detalhada que pode ser ajustada conforme necessário. Pode incluir informações adicionais e detalhes relevantes para o conteúdo apresentado.",
 };
 
 const ALIGNMENT = {
@@ -69,9 +68,10 @@ export default function LabelText(
         <div class="w-full  flex justify-center pt-10 fade-in-down">
             <div class="flex flex-col md:flex-row  gap-5 md:gap-1 md:items-center flex-wrap xl:flex-nowrap text-neutral w-[90%] xl:w-[85%] md:justify-between">
                 <div class="flex flex-col md:flex-row md:items-start md:justify-between flex-grow md:flex-wrap">
-                    <p class="pl-5 md:pl-9 border-l-[5px] border-neutral w-fit text-[32px] font-light uppercase md:max-w-1/2 tracking-[.2em]">
-                        {title}
-                    </p>
+                    <div
+                        dangerouslySetInnerHTML={{ __html: title }}
+                        class={`pl-5 md:pl-9 border-l-[5px] border-neutral w-fit font-light ${description && "md:max-w-1/2"} tracking-[.2em]`}
+                    />
                     <div
                         class={`md:pl-9 tracking-widest md:w-1/2 ${
                             GROWTH[growth]
@@ -79,9 +79,11 @@ export default function LabelText(
                             SIZE[size]
                         } ${ALIGNMENT[alignment]}`}
                     >
-                        <p>
-                            {description}
-                        </p>
+                        {description && (
+                            <p>
+                                {description}
+                            </p>
+                        )}
                     </div>
                 </div>
                 {button && button.href && button.text && (
